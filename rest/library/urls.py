14b89e665e8398e25647af3c98608fc221ceb579
Zverina1337 <80909276+Zverina1337@users.noreply.github.com>
2022-12-22 10:20:56 +0700

    full work

--- rest/library/urls.py
+++ rest/library/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
-from .views import AuthorViewSet, BookViewSet
+from .views import *
 
 
 urlpatterns = [
-    path('authors/', AuthorViewSet.as_view({'get': 'list'})),
-    path('author/<int:pk>', AuthorViewSet.as_view({'get': ''})),
+    path('authors/', AuthorViewSetReadOnly.as_view({'get': 'list'})),
+    path('author/<int:pk>', AuthorViewSetReadOnly.as_view({'get': 'retrieve'})),
+    path('author/create/', AuthorViewSet.as_view({'post': 'create'})),
 
-
-    path('books/', BookViewSet.as_view({'get': 'list'})),
-    path('book/<int:pk>', BookViewSet.as_view({'get': ''}))
+    path('books/', BookViewSetReadOnly.as_view({'get': 'list'})),
+    path('book/<int:pk>', BookViewSetReadOnly.as_view({'get': 'retrieve'})),
+    path('book/create/', BookViewSet.as_view({'post': 'create'}))
 
 ]
--- rest/library/views.py
+++ rest/library/views.py
@@ -3,11 +3,45 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from library.serializers import AuthorSerializer, BookSerializer
 from .models import Author, Book
-
-class AuthorViewSet(viewsets.ModelViewSet):
+from rest_framework.permissions import IsAdminUser
+from django.core.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
+class AuthorViewSetReadOnly(viewsets.ReadOnlyModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    
+
+class BookViewSetReadOnly(viewsets.ReadOnlyModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'genre', 'author__name', 'author__lastName', 'author__middle_name']
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes=[IsAdminUser]
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            if (Book.objects.get(title=request.data['title']).publisher != request.data['publisher'] and request.data[
+                'genre'] == 'художественное произведение, переведенное с другого языка'):
+                serializer.is_valid()
+            if (Book.objects.get(title=request.data['title']).yearofRel != request.data['yearOfRel'] and request.data[
+                'genre'] == 'учебник'):
+                serializer.is_valid()
+            else:
+                raise ValidationError(self.errors)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes=[IsAdminUser]
